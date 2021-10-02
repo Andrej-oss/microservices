06bf10ad78b532b80f6d0c2d7bd832ddc2fe1fa1
Andrej-oss <snowtomsawyer@gmail.com>
2021-10-02 23:32:54 +0200

    - added api GatewayServiceApplication and config service

---
+++ api-gateway-service/src/main/java/com/example/apigatewayservice/FallBackMethodController.java
@@ -0,0 +1,19 @@
+package com.example.apigatewayservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBack(){
+        return "User service taking more time then Expected." +
+                "Try again later.";
+    }
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBack(){
+        return "Department service taking more time then Expected." +
+                "Try again later.";
+    }
+}
